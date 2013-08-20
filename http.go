45228ff098af9f3a24096c9ccab953fdfd9d449f
cyball <francisc.simon@evalgo.com>
2013-08-20 04:15:20 +0200

    updated evmail to work with v 0.0.3

--- http.go
+++ http.go
@@ -8,12 +8,9 @@ package evmail
 
 import (
 	"github.com/evalgo/everror"
-	"github.com/evalgo/evlog"
 	"github.com/evalgo/evmessage"
-	"io/ioutil"
-	"net/http"
-	//"path/filepath"
-	//"strings"
+	"path/filepath"
+	"strings"
 )
 
 var Version string = "v1"
@@ -24,62 +21,25 @@ func NewHttpRpcProxy() *HttpRpcProxy {
 	return new(HttpRpcProxy)
 }
 
-func (rpcP *HttpRpcProxy) createSendMail(r *http.Request) (*evmessage.Message, *evmessage.Message, string, error) {
-	requestMsg, responseMsg := evmessage.RpcClientInitialize("evmail")
-	request, err := requestMsg.Body("requests").(*evmessage.Requests).ById("evmail")
-	if err != nil {
-		responseMsg.Body("errors").(*evmessage.Errors).Append(everror.NewFromError(err, everror.ERROR))
-		return nil, responseMsg, "", everror.NewFromError(err, everror.ERROR)
+func (rpcP *HttpRpcProxy) HttpCreateRequest(vars map[string]string, req *evmessage.Request) (*evmessage.Request, error) {
+	r := req.Body("http").(*evmessage.Http)
+	extension := filepath.Ext(vars["action"])
+	requestUrlPath := strings.Replace(strings.Join([]string{vars["version"], vars["service"], vars["action"]}, "/"), extension, "", 1)
+	switch requestUrlPath {
+	case Version + "/evemail/emails", "/evemail/emails":
+		switch r.Method {
+		case "POST":
+			keyValuesI := evmessage.NewKeyValues()
+			keyValuesI.ObjName = "rpc-request"
+			keyValuesI.Append("RPC_ServiceName", "evemail-rpc")
+			keyValuesI.Append("RPC_FuncName", "Email.RpcSend")
+			req.AppendToBody(keyValuesI)
+			return req, nil
+		default:
+			return nil, everror.New("the given request method+" + r.Method + " is not supported")
+		}
+	default:
+		return nil, everror.New("URL path <" + r.URI + "> is not supported!")
 	}
-	if r.FormValue("request_id") == "" {
-		err = everror.New("request_id is empty for service  createSendMail")
-		responseMsg.Body("errors").(*evmessage.Errors).Append(everror.NewFromError(err, everror.ERROR))
-		return nil, responseMsg, "", everror.NewFromError(err, everror.ERROR)
-	}
-	request.Id = r.FormValue("request_id")
-	kvs := evmessage.NewKeyValues()
-
-	kvs.Append("user", r.FormValue("user"))
-	kvs.Append("password", r.FormValue("password"))
-	kvs.Append("server", r.FormValue("server"))
-	kvs.Append("port", r.FormValue("port"))
-	kvs.Append("to", r.FormValue("to"))
-	kvs.Append("from", r.FormValue("from"))
-	kvs.Append("subject", r.FormValue("subject"))
-	kvs.Append("message", r.FormValue("message"))
-
-	files := evmessage.NewFiles()
-	for key, _ := range r.MultipartForm.File {
-		evlog.Println(key)
-		f := evmessage.NewFile()
-		File, FileHeader, _ := r.FormFile(key)
-		f.Name = FileHeader.Filename
-		fContent, _ := ioutil.ReadAll(File)
-		f.Content = fContent
-		f.EncodeBase64()
-		files.Append(f)
-	}
-
-	request.AppendToBody(files)
-	request.AppendToBody(kvs)
-	requestMsg.Body("requests").(*evmessage.Requests).InProgress = request
-	return requestMsg, responseMsg, "Email.RpcSend", nil
-}
-
-func (rpcP *HttpRpcProxy) HttpCreateRequest(vars map[string]string, r *evmessage.Request) (*evmessage.Request, error) {
-	// requestUrlPath := strings.TrimRight(r.URL.Path, "/")
-	// extension := filepath.Ext(requestUrlPath)
-	// requestUrlPath = strings.Replace(requestUrlPath, extension, "", 1)
-	// switch requestUrlPath {
-	// case "/" + Version + "/evemail/emails", "/evemail/emails":
-	// 	switch r.Method {
-	// 	case "POST":
-	// 		return mail.createSendMail(r)
-	// 	default:
-	// 		return nil, nil, "", everror.New("the given request method+" + r.Method + " is not supported")
-	// 	}
-	// default:
-	// 	return nil, nil, "", everror.New("URL path <" + r.URL.Path + "> is not supported!")
-	// }
-	return r, nil
+	return req, nil
 }
--- rpc.go
+++ rpc.go
@@ -8,6 +8,7 @@ import (
 	"github.com/evalgo/everror"
 	"github.com/evalgo/evlog"
 	"github.com/evalgo/evmessage"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
@@ -28,59 +29,68 @@ func (mail *Email) RpcSend(requestMsg *evmessage.Message, responseMsg *evmessage
 	evlog.Println("reset all errors...")
 	everror.ResetAllErrors()
 	evlog.Println("starting send ...")
+	evlog.Println(requestMsg.Body("requests").(*evmessage.Requests).InProgress.Body("http"))
 	responseMsg, err := evmessage.RpcServiceInitialize(requestMsg, responseMsg)
 	if err != nil {
-		responseMsg.Body("errors").(*evmessage.Errors).Append(everror.NewFromError(err, everror.ERROR))
 		return everror.NewFromError(err, everror.ERROR)
 	}
 	request := requestMsg.Body("requests").(*evmessage.Requests).InProgress
 	respObj := evmessage.NewResponse()
 	if request == nil {
-		err = everror.New("there is no Requests.InProgress request set!")
-		responseMsg.Body("errors").(*evmessage.Errors).Append(everror.NewFromError(err, everror.ERROR))
-		return everror.NewFromError(err, everror.ERROR)
+		return everror.New("there is no Requests.InProgress request set!")
 	}
 	respObj.Id = request.Id
 	respObj.Order = request.Order
-	kValues := request.Body("keyvalues").(*evmessage.KeyValues)
+	kValues := requestMsg.Body("requests").(*evmessage.Requests).InProgress.Body("http").(*evmessage.Http)
 	email := NewEmail()
-	email.User = kValues.ByKey("user")
-	email.Password = kValues.ByKey("password")
-	email.Server = kValues.ByKey("server")
-	port, err := strconv.Atoi(kValues.ByKey("port"))
+	email.User = kValues.FormValue("user")
+	email.Password = kValues.FormValue("password")
+	email.Server = kValues.FormValue("server")
+	port, err := strconv.Atoi(kValues.FormValue("port"))
 	if err != nil {
-		responseMsg.Body("errors").(*evmessage.Errors).Append(everror.NewFromError(err, everror.ERROR))
 		return everror.NewFromError(err, everror.ERROR)
 	}
 	email.Port = port
-	email.To = strings.Split(kValues.ByKey("to"), " ")
-	email.From = kValues.ByKey("from")
-	email.Subject = kValues.ByKey("subject")
-	email.Body = kValues.ByKey("message")
-	files := request.Body("files").(*evmessage.Files)
-	if files != nil {
-		for _, f := range files.Files {
-			evlog.Println("sending attachment:", f.Name)
-			f.DecodeBase64()
-			err := f.WriteFile("/tmp")
-			if err != nil {
-				responseMsg.Body("errors").(*evmessage.Errors).Append(everror.NewFromError(err, everror.ERROR))
-				return everror.NewFromError(err, everror.ERROR)
-			}
-			err = email.Attach("/tmp/" + f.Name)
-			if err != nil {
-				responseMsg.Body("errors").(*evmessage.Errors).Append(everror.NewFromError(err, everror.ERROR))
-				return everror.NewFromError(err, everror.ERROR)
-			}
+	email.To = strings.Split(kValues.FormValue("to"), " ")
+	email.From = kValues.FormValue("from")
+	email.Subject, err = kValues.FormValueBase64("subject")
+	email.Body, err = kValues.FormValueBase64("message")
+
+	for _, f := range kValues.Files {
+		evlog.Println("sending attachment:", f.Name)
+		f.DecodeBase64()
+		err := f.WriteFile("/tmp")
+		if err != nil {
+			return everror.NewFromError(err, everror.ERROR)
+		}
+		err = email.Attach("/tmp/" + f.Name)
+		if err != nil {
+			return everror.NewFromError(err, everror.ERROR)
 		}
 	}
+
 	err = email.Send()
 	if err != nil {
-		responseMsg.Body("errors").(*evmessage.Errors).Append(everror.NewFromError(err, everror.ERROR))
 		return everror.NewFromError(err, everror.ERROR)
 	}
+	mimeType := filepath.Ext(kValues.URI)
 	keyValues := evmessage.NewKeyValues()
-	keyValues.Append("Message", "Email was sent successfully")
+	keyValues.ObjName = "response"
+	switch mimeType {
+	case ".txt":
+		keyValues.Append("response.mime.type", "text/plain")
+		keyValues.Append("response.content", "email was sent successfully")
+	case ".html":
+		keyValues.Append("response.mime.type", "text/html")
+		keyValues.Append("response.content", "email was sent successfully")
+	case ".json":
+		keyValues.Append("response.mime.type", "application/json")
+		keyValues.AppendBase64("response.content", "{response:{message:\"email was sent successfully\"}}")
+	default:
+		keyValues.Append("response.mime.type", "text/xml")
+		keyValues.Append("response.content", "<response><message>email was sent successfully</message></response>")
+
+	}
 	respObj.AppendToBody(keyValues)
 	responseMsg.Body("responses").(*evmessage.Responses).Append(respObj)
 	return nil
